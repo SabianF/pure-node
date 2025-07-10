f3dc65b90bc6d39ec79e05f8a3ebef693d7ad3b5
SabianF <58588133+SabianF@users.noreply.github.com>
2025-07-09 20:02:18 -0700

    feat: endware implementation

--- src/data/sources/router.js
+++ src/data/sources/router.js
@@ -19,9 +19,18 @@ export default class Router {
    */
   handlers;
 
+  /**
+   * @type {handleError[]}
+   */
+  endware;
+
+
   constructor() {
     this.middleware = [];
     this.handlers = [];
+    this.endware = [];
+
+    this.endware.push(handleNotFound);
   }
 
   /**
@@ -102,13 +111,10 @@ export default class Router {
         break;
       }
 
-      if (was_handled) {
-        return;
+      for (let i = 0; i < this.endware.length; i++) {
+        const endware_item = this.endware[i];
+        endware_item(null, request, response);
       }
-
-      response.statusCode = HttpStatusCodes.codes.NOT_FOUND;
-      response.write(HttpStatusCodes.getReason(HttpStatusCodes.codes.NOT_FOUND));
-      response.end();
     });
 
     server.listen(port, listener_handler);
@@ -221,3 +227,39 @@ function validateUrl(url) {
  * @param {http.ServerResponse<http.ClientRequest>} response
  */
 export async function handleRequest(request, response) { }
+
+/**
+ *
+ * @param {Error} error
+ * @param {http.ClientRequest} request
+ * @param {http.ServerResponse<http.ClientRequest>} response
+ */
+export async function handleError(error, request, response) { }
+
+/**
+ *
+ * @param {Error} error
+ * @param {http.ClientRequest} request
+ * @param {http.ServerResponse<http.ClientRequest>} response
+ */
+async function handleNotFound(error, request, response) {
+  if (!error) {
+    error = new Error(HttpStatusCodes.reasons.NOT_FOUND);
+  }
+
+  response.statusCode = HttpStatusCodes.codes.NOT_FOUND;
+  response.write(`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"  content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible"  content="ie=edge">
+        <title>${error.message}</title>
+      </head>
+      <body>
+        <h1>${error.message}: ${request.url}</h1>
+      </body>
+    </html>
+  `);
+}
