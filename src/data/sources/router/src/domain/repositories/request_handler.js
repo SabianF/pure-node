93b7a12cbfe9dd73ac1ae04b68b89a3e0419bc7b
SabianF <58588133+SabianF@users.noreply.github.com>
2025-07-13 03:03:48 -0700

    refactor: request_handler fn extract

--- src/data/sources/router/src/domain/repositories/request_handler.js
+++ src/data/sources/router/src/domain/repositories/request_handler.js
@@ -20,35 +20,18 @@ export default function createRequestHandler(handlers, endware) {
    * @param {http.ServerResponse<http.ClientRequest>} response
    */
   const request_handler = async (request, response) => {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+
     const normalized_url = validateRequestUrl(request.url, response);
     const normalized_method = validateRequestMethod(request.method, response);
 
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
-
-    for (let i = 0; i < handlers.length; i++) {
-      const handler = handlers[i];
-
-      if (response.writableEnded) {
-        break;
-      }
-
-      if (handler.is_middleware) {
-        await handler.handler_function(request, response);
-        continue;
-      }
-
-      if (handler.url !== normalized_url) {
-        continue;
-      }
-
-      if (handler.method !== normalized_method) {
-        continue;
-      }
-
-      await handler.handler_function(request, response);
-      response.end();
-      break;
-    }
+    await executeHandlers(
+      handlers,
+      normalized_url,
+      normalized_method,
+      request,
+      response,
+    );
 
     executeEndware(request, response);
 
@@ -61,4 +44,45 @@ export default function createRequestHandler(handlers, endware) {
   };
 
   return request_handler;
+}
+
+/**
+ *
+ * @param {Handler[]} handlers
+ * @param {string} normalized_url
+ * @param {string} normalized_method
+ * @param {http.ClientRequest} request
+ * @param {http.ServerResponse<http.ClientRequest>} response
+ */
+async function executeHandlers(
+  handlers,
+  normalized_url,
+  normalized_method,
+  request,
+  response,
+) {
+  for (let i = 0; i < handlers.length; i++) {
+    const handler = handlers[i];
+
+    if (response.writableEnded) {
+      break;
+    }
+
+    if (handler.is_middleware) {
+      await handler.handler_function(request, response);
+      continue;
+    }
+
+    if (handler.url !== normalized_url) {
+      continue;
+    }
+
+    if (handler.method !== normalized_method) {
+      continue;
+    }
+
+    await handler.handler_function(request, response);
+    response.end();
+    break;
+  }
 }
