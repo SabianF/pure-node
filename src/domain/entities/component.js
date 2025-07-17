f3f6dfcd8d0554065bc8919e6394aa8dc2042ddd
SabianF <58588133+SabianF@users.noreply.github.com>
2025-07-16 20:43:34 -0700

    refactor: simplified component entity

--- src/domain/entities/component.js
+++ src/domain/entities/component.js
@@ -4,27 +4,35 @@
  * @property {Component | object} placeholders
  */
 
+/**
+ * A representation of a UI element
+ */
 export default class Component {
   /**
-   * @type {string}
-   */
-  name;
-
-  /**
-   * @type {Component | object}
-   */
-  placeholders;
-
-  /**
-   *
    * @param {ComponentProps} props
    */
   constructor({ name, placeholders }) {
-    if (!name || typeof name !== "string") {
-      throw new Error("No name string provided to Component");
-    }
-
-    this.name = name;
-    this.placeholders = placeholders;
+    this.name = validateName(name);
+    this.placeholders = validatePlaceholders(placeholders);
   }
+}
+
+function validateName(name) {
+  const message = `string for [name] provided to [${Component.name}].`;
+
+  if (typeof name !== "string") {
+    throw new Error(`Invalid ${message}`);
+  }
+
+  if (name.length === 0) {
+    throw new Error(`Empty ${message}`);
+  }
+
+  return name;
+}
+
+function validatePlaceholders(placeholders) {
+
+
+  return placeholders;
 }
