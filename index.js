db552af685593eac719fdaea23ce290fc28c5c60
SabianF <58588133+SabianF@users.noreply.github.com>
2025-07-22 01:17:04 -0700

    refactor: rm unused file

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import DataRepos from "./src/common/data/repositories/repositories.js";
 import DomainRepos from "./src/common/domain/repositories/repositories.js";
 
 // TODO(caching): implement caching
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/Conditional_requests
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Cache-Control
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/Conditional_requests
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Cache-Control
 
 // TODO(server-mocking): try out request interception: https://medium.com/@maulanamaleek/intercept-http-request-using-serviceworker-b6ef23f97d1f
-  // https://blog.logrocket.com/intercepting-javascript-fetch-api-requests-responses/
+// https://blog.logrocket.com/intercepting-javascript-fetch-api-requests-responses/
 
 // TODO(file-mgmt): impl basic file upload, storage, display
 
