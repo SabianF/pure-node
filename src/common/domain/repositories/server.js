cba7ee05202d7c2ea2d27e43fe74f9aa434883e0
SabianF <58588133+SabianF@users.noreply.github.com>
2025-07-21 17:14:12 -0700

    refactor: single loadPage usecase

--- src/common/domain/repositories/server.js
+++ src/common/domain/repositories/server.js
@@ -1,6 +1,8 @@
-import loadBlankPage from "../usecases/load_blank_page.js";
-import loadHomePage from "../usecases/load_home_page.js";
-import loadTestPage from "../usecases/load_test_page.js";
+import loadPage from "../usecases/load_page.js";
+
+/**
+ * @typedef {import("../entities/types.js").HttpRequestHandler} HttpRequestHandler
+ */
 
 /**
  * @typedef {import("../entities/types.js").DataRepos} DataRepos
@@ -30,16 +32,22 @@ export default class ServerRepo {
    * @param {import("../entities/types.js").Router} router
    */
   addPublicRoutes(router) {
-    router.get("/", loadHomePage({
-      rendering_repo: this.#data_repos.rendering,
-    }));
+    router.get("/", async (request, response) => {
+      const home_page = loadPage("ROOT");
+      const rendered_home_page = await this.#data_repos.rendering.renderPage(home_page);
+      response.writeHtml(rendered_home_page);
+    });
 
-    router.get("/test", loadTestPage({
-      rendering_repo: this.#data_repos.rendering,
-    }));
+    router.get("/test", async (request, response) => {
+      const test_page = loadPage("TEST");
+      const rendered_test_page = await this.#data_repos.rendering.renderPage(test_page);
+      response.writeHtml(rendered_test_page);
+    });
 
-    router.get("/blank", loadBlankPage({
-      rendering_repo: this.#data_repos.rendering,
-    }));
+    router.get("/blank", async (request, response) => {
+      const blank_page = loadPage("BLANK");
+      const rendered_blank_page = await this.#data_repos.rendering.renderPage(blank_page);
+      response.writeHtml(rendered_blank_page);
+    });
   }
 }
