ce7a4e5a8fba846aa6ef05c27314eb30c280cfbb
SabianF <58588133+SabianF@users.noreply.github.com>
2025-07-28 23:12:55 -0700

    style: type comments cleanup

--- src/common/domain/entities/types.js
+++ src/common/domain/entities/types.js
@@ -1,58 +1,20 @@
 /**
  * @typedef {import("./component.js").default} Component
- */
-
-/**
+ * @typedef {import("./page.js").default} Page
+ * @typedef {import("../repositories/repositories.js").default} DomainRepos
+ * @typedef {import("../repositories/server.js").ServerRepo} RoutesRepo
+ * @typedef {import("../../data/repositories/repositories.js").default} DataRepos
+ * @typedef {import("../../domain/repositories/repositories.js").default} DomainRepos
+ * @typedef {import("../../data/repositories/rendering.js").default} RenderingRepo
+ * @typedef {import("../../data/repositories/file_system.js").default} FileSystemRepo
+ * @typedef {import("../../data/sources/file_system_lib.js").default} FileSystemLib
+ * @typedef {import("../../data/sources/router/src/domain/entities/types.js").HttpRequestHandler} HttpRequestHandler
+ * @typedef {import("../../data/sources/router/src/domain/entities/types.js").ResponseModel} HttpResponse
+ * @typedef {import("../../data/sources/router/src/domain/entities/types.js").HttpRequest} HttpRequest
+ * @typedef {import("../../data/sources/router/index.js").Router} Router
+ * 
  * @typedef ComponentString
  * @property {string} html
  * @property {string} css
  * @property {string} js
- */
-
-/**
- * @typedef {import("./page.js").default} Page
- */
-
-/**
- * @typedef {import("../repositories/repositories.js").default} DomainRepos
- */
-
-/**
- * @typedef {import("../repositories/server.js").ServerRepo} RoutesRepo
- */
-
-/**
- * @typedef {import("../../data/repositories/repositories.js").default} DataRepos
- */
-
-/**
- * @typedef {import("../../domain/repositories/repositories.js").default} DomainRepos
- */
-
-/**
- * @typedef {import("../../data/repositories/rendering.js").default} RenderingRepo
- */
-
-/**
- * @typedef {import("../../data/repositories/file_system.js").default} FileSystemRepo
- */
-
-/**
- * @typedef {import("../../data/sources/file_system_lib.js").default} FileSystemLib
- */
-
-/**
- * @typedef {import("../../data/sources/router/src/domain/entities/types.js").HttpRequestHandler} HttpRequestHandler
- */
-
-/**
- * @typedef {import("../../data/sources/router/src/domain/entities/types.js").ResponseModel} HttpResponse
- */
-
-/**
- * @typedef {import("../../data/sources/router/src/domain/entities/types.js").HttpRequest} HttpRequest
- */
-
-/**
- * @typedef {import("../../data/sources/router/index.js").Router} Router
  */
